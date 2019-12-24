d5c87efafb9fca5818aa9ac16089ea413a8bae3b
xujian <f001872b923f4d63a85c7fde313af7d8@sino.com>
2019-12-24 14:46:25 +0800

    定义全局异常

---
+++ src/main/java/com/sinosoft/cses/response/BaseResponse.java
@@ -0,0 +1,57 @@
+package com.sinosoft.cses.response;
+
+
+import com.sinosoft.cses.enums.ErrorType;
+
+/**
+ * @program: workspace
+ * @description: 系统基本返回类型
+ * @author: xujian
+ * @create: 2019-08-27 10:34
+ **/
+public class BaseResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public BaseResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public BaseResponse(T data, ErrorType errorType){
+        this.code=errorType.getCode();
+        this.message=errorType.getMessage();
+        this.data=data;
+    }
+
+    public BaseResponse(ErrorType errorType){
+        this.code=errorType.getCode();
+        this.message=errorType.getMessage();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
