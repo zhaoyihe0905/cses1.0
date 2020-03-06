ad1f2f2d51937271cf35991ecd5f852b6729bf20
xujian <xj143068@163.com>
2020-03-06 16:15:45 +0800

    取出junte test 包

--- src/main/java/com/sinosoft/cses/util/BusinessFun.java
+++ src/main/java/com/sinosoft/cses/util/BusinessFun.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
